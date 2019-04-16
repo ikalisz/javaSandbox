81acf631e748b209ca0e572a7874c1726f41a460
Ian Kaliszewski <ikalisz@yahoo.com>
2019-04-16 11:07:29 -0600

    added more notes on if else

--- dayTwo.js
+++ dayTwo.js
@@ -7,14 +7,28 @@ if (password === confirmPassword) {
 }
 
 // === does not equal ==
-var userId = 26
-var id = '26'
+// var userId = 26
+// var id = '26'
 // == is not strict, looks for value instead of data type
 //== will return if, === will return else statement
-if (+userId == +id) {
-    console.log('we have found the user we want')
-    var found = true
+//!= acts the same as ==, !== acts the same as === however check if the value or data type is not true
+
+// if (+userId == +id) {
+//     console.log('we have found the user we want')
+//     var found = true
+// } else {
+//     console.log('the id we have does not match the user id')
+//     var found = false
+// }
+
+// || an or statement, one can be true
+// && and statement, all have to be true
+
+var hasDriversLicense = true
+var age = 36
+
+if (age >= 26 && hasDriversLicense) {
+    console.log('you can rent a car with us')
 } else {
-    console.log('the id we have does not match the user id')
-    var found = false
+    console.log('you can\'t rent a car with us')
 }
