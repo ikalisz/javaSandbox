92852fc643a382a3bd8a8e30ca79f9b1d3c64f9a
Ian Kaliszewski <ikalisz@yahoo.com>
2019-04-17 09:16:00 -0600

    Added dayThree.js and added notes on whiteboarding

---
+++ dayThree.js
@@ -0,0 +1,25 @@
+//whiteboarding
+//Wrtie a function that takes in a number.
+    //if the number is divisible by three, return 'fizz'
+    //if the number is divisible by five, return 'buzz'
+    //if the number is divisible by five and three, return 'fizz buzz'
+    //if the number is not divisible by three or five, return the number
+
+//Step One: Clarify the question
+
+//Step Two: Create sample data
+    //5 => 'buzz'
+    //3 => 'fizz'
+    //15 => 'fizz buzz'
+
+//Step Three: Define code constructs aka define what code you want to use
+    //function
+    //if/else statement
+    //modules => %
+
+//Step Four: Psuedo Code, write steps you will take to solve
+    // write a function that takes in parameter number
+    // use an if else statement to check if number is divisible by 3 or 5 with no remainder
+    //function will return statements based on if number is divisible by 3 or 5
+
+//Step Five: Code
