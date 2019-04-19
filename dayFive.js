6fe74d9603cf360b0d75b827a6fb2d5a5e63f58d
Ian Kaliszewski <ikalisz@yahoo.com>
2019-04-19 11:25:26 -0600

    added notes on spread

--- dayFive.js
+++ dayFive.js
@@ -29,4 +29,36 @@ for (let prop in obj) {
 }
 /* for in loops loop through an object or an array and for each item execute code assigning it to the variable at
 the start of the condition */
-//array.map() builds a new array and stores the values that get returned from the function in the new array from the old array
+//array.map(element, index, array) builds a new array and stores the values that get returned from the function in the new array from the old array
+//element, index, array parameters always represent element index and array in that order
+
+//array.reduce(total, element, index, array) will go through every item and reduce all values to one value by the function specified
+let arrNumbers = [1,2,3,4,5]
+
+let sum = arrNumbers.reduce(function(total, element, index, array) {
+    return total + element
+})
+
+console.log(sum)
+
+//Object Destructuring
+
+let myObj = {
+    nested: {
+        name: 'joe',
+        likesCode: true,
+        job: 'DM',
+        eyes: 'blue',
+        shoes: 'black'
+    }
+}
+
+let {name, likesCode, job, eyes, shoes} = myObj.nested
+
+console.log(name)
+
+//[...arrayName] makes a copy of the array and stores it in the array
+let nums = [1,2,3,4, 'pizza']
+let numsCopy = ['first', ...nums, 'last']
+//or
+let numsCopy2 = [...nums]
